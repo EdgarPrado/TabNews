91a487d45b8daa9f740bdc79c00e94e6375f9df0
Edgar Prado <118636914+EdgarPrado@users.noreply.github.com>
2025-12-01 10:03:52 +0000

    Minha mensagem no Deploy

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 function Home() {
     return <h1>
-        Eai mãe, Bom dia! Vai comprar pão?
+        O seu potencial é enorme! Nunca se esqueça disso.
+        E se algum dia se esquecer, lembre-se que estou aqui contigo.
+        Beijo pitica!
     </h1>
 }
 
