6ad796c494a25d354aad63466fecd287dc8a8770
Edgar Prado <118636914+EdgarPrado@users.noreply.github.com>
2025-12-03 11:14:56 +0000

    :fix: Ajeitando o problema de desatualizacao do npm e mudando o texto

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 function Home() {
-    return <h1>
-        O seu potencial é enorme! Nunca se esqueça disso.
-        E se algum dia se esquecer, lembre-se que estou aqui contigo.
-        Beijo pitica!
-    </h1>
+    return (
+    <div>
+    <h1> O seu potencial é enorme! </h1> 
+    <p> Nunca se esqueça disso. </p>
+    <p> E se algum dia se esquecer, lembre-se que estou aqui contigo.</p>    
+    <p> Beijo pitica! 💜</p>
+    </div>
+    ) 
 }
 
 export default Home;
